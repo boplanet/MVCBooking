5db5cef6a868fb0d97c7328793061eec63b08be4
boplanet <boplanet@gmail.com>
2023-11-03 20:10:12 +0100

    Add project files.

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+using System.Diagnostics;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+       
+    {
+        private static List<HotelBooking> Rezervacije = new List<HotelBooking>();
+        public IActionResult Index()
+        {
+            return View(Rezervacije);
+        }
+        public IActionResult Create()
+        {
+            HotelBooking rezervacija = new HotelBooking();
+            return View(rezervacija);
+        }
+        public IActionResult CreateBooking(HotelBooking rezervacijaViewModel)
+        {
+            Rezervacije.Add(rezervacijaViewModel);
+            return RedirectToAction("Create");
+            //  return RedirectToAction("Index");
+        }
+
+    }
+}
